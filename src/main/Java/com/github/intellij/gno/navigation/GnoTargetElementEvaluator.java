a07cb39bca5b80153f251e53ad6d564d44e3114a
DIGIX666 <digixit66@gmail.com>
2025-03-17 13:56:17 +0100

    Revert "Merge pull request #17 from gnoverse/fixbug/pathGnopls"
    This reverts commit b6000bcf49aabce4ebc040072e6f418c0a030aec, reversing
    changes made to 0bce0ef0781f21e3a9b0a6e50b9f3a0d3a53be46.

---
+++ src/main/Java/com/github/intellij/gno/navigation/GnoTargetElementEvaluator.java
@@ -0,0 +1,33 @@
+package com.github.intellij.gno.navigation;
+
+import com.intellij.codeInsight.TargetElementEvaluatorEx2;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.Nullable;
+
+public class GnoTargetElementEvaluator extends TargetElementEvaluatorEx2 {
+
+
+    public @Nullable PsiElement getElementAt(@Nullable PsiElement element) {
+        if (element == null) return null;
+
+        PsiElement referenceElement = PsiTreeUtil.getNonStrictParentOfType(element, PsiElement.class);
+
+        if (referenceElement != null && isValidTarget(referenceElement)) {
+            return referenceElement;
+        }
+        return null;
+    }
+
+    private boolean isValidTarget(PsiElement element) {
+        if (element.getNode() == null) return false;
+
+        String elementType = element.getNode().getElementType().toString();
+
+        if (elementType.contains("COMMENT") || elementType.contains("STRING") || elementType.contains("WHITE_SPACE")) {
+            return false;
+        }
+
+        return elementType.equals("IDENTIFIER") && element.getReference() != null;
+    }
+}
